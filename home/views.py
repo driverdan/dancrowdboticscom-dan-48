763ce6c2b5ea3f29488a6b62b077e50d2e5316a9
Dan DeFelippi <dan@driverdan.com>
2018-12-06 18:45:48 +0000

    add package django-bootstrap4

--- home/views.py
+++ home/views.py
@@ -7,6 +7,9 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-bootstrap4', 'url': 'https://pypi.org/project/django-bootstrap4/0.0.7/'},
+	{'name':'djangorestframework', 'url': 'https://pypi.org/project/djangorestframework/3.9.0/'},
+	{'name':'django-allauth', 'url': 'https://pypi.org/project/django-allauth/0.38.0/'},
     ]
     context = {
         'customtext': CustomText.objects.first(),
